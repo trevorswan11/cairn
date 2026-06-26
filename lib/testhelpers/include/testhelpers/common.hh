08dc89598b8684fdef4ffa7eb3818ad80faf6247
trevorswan11 <tcs94@case.edu>
2026-06-25 22:30:39 -0400

    test: Fully test disk manager, successfully completing ACs in #17

--- lib/testhelpers/include/testhelpers/common.hh
+++ lib/testhelpers/include/testhelpers/common.hh
@@ -16,15 +16,7 @@ concept Unwrappable = stdx::Option<std::remove_cvref_t<T>> ||
 // Unpacks the value in the option or result and returns its value if present
 template <Unwrappable U> [[nodiscard]] auto unwrap(U&& u) -> decltype(auto) {
     REQUIRE(u);
-    return *u;
-}
-
-// Unpacks the value in the option or result and returns its value if present and equal to expected
-template <Unwrappable U, typename E>
-[[nodiscard]] auto unwrap(U&& u, E&& expected_value) -> decltype(auto) {
-    REQUIRE(u);
-    REQUIRE(*u == std::forward<E>(expected_value));
-    return *u;
+    return *std::forward<U>(u);
 }
 
 template <Unwrappable U> auto unwrap_err(U&& u) -> decltype(auto) {
