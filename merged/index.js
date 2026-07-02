1ab84eeaddae840d2b8e2a7ffb5d0e674153c5be
trevorswan11 <tcs94@case.edu>
2026-07-02 02:41:59 +0000

    Deploying to coverage from @ trevorswan11/cairn@6320a9740f5715877c3058a8c96175bfa9c4cf21 🚀

---
+++ merged/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"98.5","covered_lines":"986","uncovered_lines":"15","total_lines" : "1001"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2026-07-02 02:41:58", "instrumented" : 1001, "covered" : 986,};
---
+++ merged/kcov-merged/index.js
@@ -0,0 +1,17 @@
+var data = {files:[
+{"link":"rw_latch.hh.4cfa2484.html","title":"rw_latch.hh","summary_name":"[...]/cairn/lib/storage/include/storage/rw_latch.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"4","uncovered_lines":"0","total_lines" : "4"},
+{"link":"bplus_tree.hh.e994f089.html","title":"bplus_tree.hh","summary_name":"[...]/cairn/lib/storage/include/storage/bplus_tree.hh","covered_class":"lineCov","covered":"99.8","covered_lines":"458","uncovered_lines":"1","total_lines" : "459"},
+{"link":"page.hh.ea3839a5.html","title":"page.hh","summary_name":"[...]/cairn/lib/storage/include/storage/page.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"24","uncovered_lines":"0","total_lines" : "24"},
+{"link":"slotted_page.hh.8a693b9b.html","title":"slotted_page.hh","summary_name":"[...]/cairn/lib/storage/include/storage/slotted_page.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"17","uncovered_lines":"0","total_lines" : "17"},
+{"link":"config.cc.65e92031.html","title":"config.cc","summary_name":"[...]/cairn/lib/support/src/config.cc","covered_class":"lineCov","covered":"100.0","covered_lines":"23","uncovered_lines":"0","total_lines" : "23"},
+{"link":"frame_replacer.hh.68e61d5f.html","title":"frame_replacer.hh","summary_name":"[...]/cairn/lib/storage/include/storage/frame_replacer.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"69","uncovered_lines":"0","total_lines" : "69"},
+{"link":"disk_manager.cc.fcb1f46a.html","title":"disk_manager.cc","summary_name":"[...]/cairn/lib/storage/src/disk_manager.cc","covered_class":"lineCov","covered":"100.0","covered_lines":"66","uncovered_lines":"0","total_lines" : "66"},
+{"link":"rw_latch.cc.78438745.html","title":"rw_latch.cc","summary_name":"[...]/cairn/lib/storage/src/rw_latch.cc","covered_class":"lineCov","covered":"100.0","covered_lines":"44","uncovered_lines":"0","total_lines" : "44"},
+{"link":"disk_manager.hh.94fcbee5.html","title":"disk_manager.hh","summary_name":"[...]/cairn/lib/storage/include/storage/disk_manager.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"10","uncovered_lines":"0","total_lines" : "10"},
+{"link":"slotted_page.cc.e2247114.html","title":"slotted_page.cc","summary_name":"[...]/cairn/lib/storage/src/slotted_page.cc","covered_class":"lineCov","covered":"93.0","covered_lines":"93","uncovered_lines":"7","total_lines" : "100"},
+{"link":"buffer_pool.hh.146f15a.html","title":"buffer_pool.hh","summary_name":"[...]/cairn/lib/storage/include/storage/buffer_pool.hh","covered_class":"lineCov","covered":"96.2","covered_lines":"177","uncovered_lines":"7","total_lines" : "184"},
+{"link":"config.hh.3d1651a.html","title":"config.hh","summary_name":"[...]/cairn/lib/support/include/support/config.hh","covered_class":"lineCov","covered":"100.0","covered_lines":"1","uncovered_lines":"0","total_lines" : "1"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2026-07-02 02:41:58", "instrumented" : 1001, "covered" : 986,};
+var merged_data = [];
