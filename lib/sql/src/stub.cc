0e528b08ee5f7c88ccbb6172e504d2b52e4ee95f
trevorswan11 <tcs94@case.edu>
2026-07-01 23:41:12 -0400

    chore: Remove stubs

--- lib/sql/src/stub.cc
+++
@@ -1,9 +0,0 @@
-#include "sql/stub.hh"
-
-#include <stdx/types.hh>
-
-namespace cairn::sql {
-
-auto some_really_complicated_work(i32 a, i32 b) noexcept -> i32 { return a + b; }
-
-} // namespace cairn::sql
