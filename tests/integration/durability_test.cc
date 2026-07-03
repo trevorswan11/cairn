3030f0a41bf7972d0460b5f028366847d14e2a9b
trevorswan11 <tcs94@case.edu>
2026-07-02 22:34:50 -0400

    test: Add smoke testing via integration and fuzz tests

---
+++ tests/integration/durability_test.cc
@@ -0,0 +1,59 @@
+#include <cstddef>
+#include <string>
+#include <string_view>
+
+#include <catch2/catch_test_macros.hpp>
+#include <gsl/span>
+#include <stdx/types.hh>
+
+#include "storage/bplus.hh"
+#include "storage/page.hh"
+#include "testhelpers/conversion.hh"
+#include "testhelpers/tempfile.hh"
+#include "testhelpers/unwrap.hh"
+
+namespace cairn::tests {
+
+using namespace cairn::storage;
+
+TEST_CASE("iot_tree durability smoke test") {
+    helpers::tempfile file{"iot_durability_smoke"};
+    using tree_t = iot_tree<i64, 128, 64>;
+    page_id_t meta{INVALID_PAGE_ID};
+
+    const std::string_view val1{"persistence test data one"};
+    const std::string      val2(120, 'd');
+    const std::string_view val3{"short test data"};
+
+    // Open pool & create tree
+    {
+        auto pool{helpers::unwrap(tree_t::pool_t::open(file.path))};
+        auto tree{helpers::unwrap(tree_t::create(*pool))};
+        meta = tree.meta_page();
+
+        REQUIRE(tree.emplace(10, helpers::span_from_string(val1)));
+        REQUIRE(tree.emplace(20, helpers::span_from_string(val2)));
+        REQUIRE(tree.emplace(30, helpers::span_from_string(val3)));
+        REQUIRE(pool->flush());
+    }
+
+    // Reopen the data files, simulating a process restart
+    {
+        auto   pool{helpers::unwrap(tree_t::pool_t::open(file.path))};
+        tree_t tree{*pool, meta};
+
+        CHECK(helpers::unwrap(
+                  tree.range_scan(10, 30, [&](const i64& k, const gsl::span<const std::byte>& v) {
+                      const auto str_v{helpers::string_from_span(v)};
+                      if (k == 10) {
+                          CHECK(str_v == val1);
+                      } else if (k == 20) {
+                          CHECK(str_v == val2);
+                      } else if (k == 30) {
+                          CHECK(str_v == val3);
+                      }
+                  })) == 3);
+    }
+}
+
+} // namespace cairn::tests
