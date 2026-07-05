cd459fa0ce9ccd2fac2ad9fe1b719c0b06059725
trevorswan11 <tcs94@case.edu>
2026-07-05 09:36:30 -0400

    test: complete apple self exe path impl

--- tests/crash/helpers/subprocess.cc
+++ tests/crash/helpers/subprocess.cc
@@ -3,23 +3,33 @@
 #include <array>
 #include <string>
 
+#include <stdx/memory.hh>
+#include <stdx/types.hh>
+
 #include <cairn/config.h>
 
 #if CAIRN_WINDOWS
 #    include <libloaderapi.h>
 #    include <minwindef.h>
 #elif CAIRN_APPLE
+#    include <mach-o/dyld.h>
 #else
 #endif
 
 namespace cairn::tests::helpers {
 
 auto self_exe_path() -> std::string {
+    using namespace stdx::size_literals;
+    std::array<char, 1_KiB> buffer;
+    buffer.fill(0);
+
 #if CAIRN_WINDOWS
-    std::array<char, MAX_PATH> buffer;
     ::GetModuleFileNameA(nullptr, buffer.data(), buffer.size());
     return buffer.data();
 #elif CAIRN_APPLE
+    u32 size{sizeof(buffer)};
+    ::_NSGetExecutablePath(buffer.data(), &size);
+    return buffer.data();
 #else
 #endif
 }
