aebc2330e08d71773186a924193a12a7fc1d5532
trevorswan11 <tcs94@case.edu>
2026-07-05 20:37:45 -0400

    test: reorganize txn tests to be separate cases

--- tests/txn/manager_test.cc
+++ tests/txn/manager_test.cc
@@ -21,96 +21,110 @@ using namespace cairn::txn;
 using namespace stdx::size_literals;
 using helpers::unwrap;
 
-TEST_CASE("txn::manager basics") {
+TEST_CASE("txn::manager begin, update LSN, commit") {
     manager           tm;
-    helpers::tempfile file{"txn_manager_test"};
+    helpers::tempfile file{"txn_manager_ops"};
     wal::log::manager lm{file.path, 1_KiB};
 
-    SECTION("begin, update LSN, commit") {
-        const auto id1{tm.begin_txn()};
-        CHECK(id1 == id_t{1});
+    const auto id1{tm.begin_txn()};
+    CHECK(id1 == id_t{1});
 
-        auto att{tm.snapshot_att()};
-        REQUIRE(att.size() == 1);
-        CHECK(att[0].txn_id == id1);
-        CHECK(att[0].state == wal::checkpoint::att_entry::state_t::ACTIVE);
-        CHECK_FALSE(att[0].last_lsn.has_value());
+    auto att{tm.snapshot_att()};
+    REQUIRE(att.size() == 1);
+    CHECK(att[0].txn_id == id1);
+    CHECK(att[0].state == wal::checkpoint::att_entry::state_t::ACTIVE);
+    CHECK_FALSE(att[0].last_lsn.has_value());
 
-        // Update LSN
-        REQUIRE(tm.update_txn_lsn(id1, wal::log::seq_num{42}));
-        att = tm.snapshot_att();
-        REQUIRE(att.size() == 1);
-        CHECK(att[0].last_lsn == wal::log::seq_num{42});
+    // Update LSN
+    REQUIRE(tm.update_txn_lsn(id1, wal::log::seq_num{42}));
+    att = tm.snapshot_att();
+    REQUIRE(att.size() == 1);
+    CHECK(att[0].last_lsn == wal::log::seq_num{42});
 
-        // Commit transaction
-        REQUIRE(tm.commit_txn(id1, lm));
-        CHECK(tm.snapshot_att().empty());
+    // Commit transaction
+    REQUIRE(tm.commit_txn(id1, lm));
+    CHECK(tm.snapshot_att().empty());
 
-        // Verify the commit record is in WAL
-        auto reader{unwrap(wal::log::reader::open(file.path))};
-        auto r{unwrap(unwrap(reader.next_record()))};
-        CHECK(r.txn_id == id1);
-        CHECK(r.type == wal::log::record_type::COMMIT);
-        CHECK(r.prev_lsn == wal::log::seq_num{42});
-    }
+    // Verify the commit record is in WAL
+    auto reader{unwrap(wal::log::reader::open(file.path))};
+    auto r{unwrap(unwrap(reader.next_record()))};
+    CHECK(r.txn_id == id1);
+    CHECK(r.type == wal::log::record_type::COMMIT);
+    CHECK(r.prev_lsn == wal::log::seq_num{42});
+}
 
-    SECTION("abort transaction") {
-        const auto id2{tm.begin_txn()};
-        CHECK(id2 == id_t{1});
+TEST_CASE("txn::manager abort transaction") {
+    manager           tm;
+    helpers::tempfile file{"txn_manager_abort"};
+    wal::log::manager lm{file.path, 1_KiB};
 
-        REQUIRE(tm.update_txn_lsn(id2, wal::log::seq_num{55}));
+    const auto id2{tm.begin_txn()};
+    CHECK(id2 == id_t{1});
 
-        // Abort transaction
-        REQUIRE(tm.abort_txn(id2, lm));
-        CHECK(tm.snapshot_att().empty());
+    REQUIRE(tm.update_txn_lsn(id2, wal::log::seq_num{55}));
 
-        // Verify the abort record is in WAL
-        auto reader{unwrap(wal::log::reader::open(file.path))};
-        auto r{unwrap(unwrap(reader.next_record()))};
-        CHECK(r.txn_id == id2);
-        CHECK(r.type == wal::log::record_type::ABORT);
-        CHECK(r.prev_lsn == wal::log::seq_num{55});
-    }
+    // Abort transaction
+    REQUIRE(tm.abort_txn(id2, lm));
+    CHECK(tm.snapshot_att().empty());
 
-    SECTION("set next txn id") {
-        tm.set_next_txn_id(id_t{100});
-        const auto id{tm.begin_txn()};
-        CHECK(id == id_t{100});
-    }
+    // Verify the abort record is in WAL
+    auto reader{unwrap(wal::log::reader::open(file.path))};
+    auto r{unwrap(unwrap(reader.next_record()))};
+    CHECK(r.txn_id == id2);
+    CHECK(r.type == wal::log::record_type::ABORT);
+    CHECK(r.prev_lsn == wal::log::seq_num{55});
+}
 
-    SECTION("transaction and commit timestamps") {
-        const auto id1{tm.begin_txn()};
-        CHECK(unwrap(tm.get_read_timestamp(id1)) == timestamp_t{0});
-        CHECK(tm.snapshot_horizon() == timestamp_t{0});
+TEST_CASE("txn::manager set next txn id") {
+    manager           tm;
+    helpers::tempfile file{"txn_manager_set_next"};
+    wal::log::manager lm{file.path, 1_KiB};
 
-        const auto id2{tm.begin_txn()};
-        CHECK(unwrap(tm.get_read_timestamp(id2)) == timestamp_t{0});
+    tm.set_next_txn_id(id_t{100});
+    const auto id{tm.begin_txn()};
+    CHECK(id == id_t{100});
+}
 
-        REQUIRE(tm.update_txn_lsn(id1, wal::log::seq_num{10}));
-        REQUIRE(tm.commit_txn(id1, lm)); // clock -> 1
+TEST_CASE("txn::manager transaction and commit timestamps") {
+    manager           tm;
+    helpers::tempfile file{"txn_manager_commits"};
+    wal::log::manager lm{file.path, 1_KiB};
 
-        CHECK(unwrap(tm.get_commit_timestamp(id1)) == timestamp_t{1});
-        CHECK(tm.snapshot_horizon() == timestamp_t{0}); // limited by active id2
-        CHECK(unwrap(tm.get_commit_timestamp(id1)) == timestamp_t{1});
+    const auto id1{tm.begin_txn()};
+    CHECK(unwrap(tm.get_read_timestamp(id1)) == timestamp_t{0});
+    CHECK(tm.snapshot_horizon() == timestamp_t{0});
 
-        REQUIRE(tm.update_txn_lsn(id2, wal::log::seq_num{20}));
-        REQUIRE(tm.commit_txn(id2, lm)); // clock -> 2
+    const auto id2{tm.begin_txn()};
+    CHECK(unwrap(tm.get_read_timestamp(id2)) == timestamp_t{0});
 
-        CHECK(unwrap(tm.get_commit_timestamp(id2)) == timestamp_t{2});
-        CHECK(tm.snapshot_horizon() == timestamp_t{2}); // no active txns
+    REQUIRE(tm.update_txn_lsn(id1, wal::log::seq_num{10}));
+    REQUIRE(tm.commit_txn(id1, lm)); // clock -> 1
 
-        CHECK(unwrap(tm.get_commit_timestamp(id1)) == INVALID_TIMESTAMP); // pruned
-        CHECK(unwrap(tm.get_commit_timestamp(id2)) == timestamp_t{2});    // not pruned
+    CHECK(unwrap(tm.get_commit_timestamp(id1)) == timestamp_t{1});
+    CHECK(tm.snapshot_horizon() == timestamp_t{0}); // limited by active id2
+    CHECK(unwrap(tm.get_commit_timestamp(id1)) == timestamp_t{1});
 
-        CHECK(helpers::unwrap_err(tm.get_read_timestamp(id1)) == error_t::TXN_NOT_FOUND);
-    }
+    REQUIRE(tm.update_txn_lsn(id2, wal::log::seq_num{20}));
+    REQUIRE(tm.commit_txn(id2, lm)); // clock -> 2
 
-    SECTION("not found / error cases") {
-        CHECK(helpers::unwrap_err(tm.commit_txn(id_t{999}, lm)) == error_t::TXN_NOT_FOUND);
-        CHECK(helpers::unwrap_err(tm.abort_txn(id_t{999}, lm)) == error_t::TXN_NOT_FOUND);
-        CHECK(helpers::unwrap_err(tm.update_txn_lsn(id_t{999}, wal::log::seq_num{123})) ==
-              error_t::TXN_NOT_FOUND);
-    }
+    CHECK(unwrap(tm.get_commit_timestamp(id2)) == timestamp_t{2});
+    CHECK(tm.snapshot_horizon() == timestamp_t{2}); // no active txns
+
+    CHECK(unwrap(tm.get_commit_timestamp(id1)) == INVALID_TIMESTAMP); // pruned
+    CHECK(unwrap(tm.get_commit_timestamp(id2)) == timestamp_t{2});    // not pruned
+
+    CHECK(helpers::unwrap_err(tm.get_read_timestamp(id1)) == error_t::TXN_NOT_FOUND);
+}
+
+TEST_CASE("txn::manager not found / error cases") {
+    manager           tm;
+    helpers::tempfile file{"txn_manager_errors"};
+    wal::log::manager lm{file.path, 1_KiB};
+
+    CHECK(helpers::unwrap_err(tm.commit_txn(id_t{999}, lm)) == error_t::TXN_NOT_FOUND);
+    CHECK(helpers::unwrap_err(tm.abort_txn(id_t{999}, lm)) == error_t::TXN_NOT_FOUND);
+    CHECK(helpers::unwrap_err(tm.update_txn_lsn(id_t{999}, wal::log::seq_num{123})) ==
+          error_t::TXN_NOT_FOUND);
 }
 
 } // namespace cairn::tests
