205f004692116a04a1e41aa41328d19ffb76a68e
trevorswan11 <tcs94@case.edu>
2026-07-06 18:42:23 -0400

    fix: Make snapshots take buffers instead of freshly allocating

--- lib/wal/include/wal/checkpoint/manager.hh
+++ lib/wal/include/wal/checkpoint/manager.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <vector>
 
 #include <stdx/result.hh>
 #include <stdx/types.hh>
@@ -8,6 +9,7 @@
 #include "storage/buffer_pool.hh"
 #include "support/error.hh"
 #include "txn/manager.hh"
+#include "wal/checkpoint/types.hh"
 #include "wal/log/manager.hh"
 #include "wal/log/record.hh"
 #include "wal/log/seq_num.hh"
@@ -26,13 +28,13 @@ class manager {
         begin_rec.type = log::record_type::CHECKPOINT_BEGIN;
         const auto begin_lsn{TRY(wal_manager.append_record(begin_rec))};
 
-        auto dpt{pool.snapshot_dpt()};
-        auto att{tm.snapshot_att()};
+        pool.snapshot_dpt(dpt_);
+        tm.snapshot_att(att_);
 
         log::record end_rec;
         end_rec.type = log::record_type::CHECKPOINT_END;
-        end_rec.dpt  = dpt;
-        end_rec.att  = att;
+        end_rec.dpt  = dpt_;
+        end_rec.att  = att_;
 
         const auto end_lsn{TRY(wal_manager.append_record(end_rec))};
         TRY(wal_manager.flush(end_lsn));
@@ -48,6 +50,8 @@ class manager {
 
   private:
     const std::filesystem::path control_path_;
+    std::vector<dpt_entry> dpt_;
+    std::vector<att_entry> att_;
 };
 
 } // namespace cairn::wal::checkpoint
