0ef52c7f6d5dabc5d22e9c104214f76978c74244
trevorswan11 <tcs94@case.edu>
2026-07-08 19:09:30 -0400

    feat: add index scan from improved read set

--- lib/txn/include/txn/read_set.hh
+++ lib/txn/include/txn/read_set.hh
@@ -4,18 +4,19 @@
 
 #include <stdx/option.hh>
 
-#include "txn/id.hh"
 #include "support/error.hh"
+#include "txn/id.hh"
 
 namespace cairn::txn {
 
 class read_set_t {
   public:
+    using get_commit_ts_t = std::function<stdx::option<timestamp_t>(id_t)>;
+
+  public:
     virtual ~read_set_t() = default;
     [[nodiscard]] virtual auto
-    check_conflict(id_t                                                  reader_id,
-                   timestamp_t                                           read_ts,
-                   const std::function<stdx::option<timestamp_t>(id_t)>& get_commit_ts) const
+    check_conflict(id_t reader_id, timestamp_t read_ts, const get_commit_ts_t& get_commit_ts) const
         -> result<bool> = 0;
 };
 
