ac94b95e6c22c901a699fecdeac712f66d327211
trevorswan11 <tcs94@case.edu>
2026-07-14 22:11:33 -0400

    fix: so much to fix here...

--- lib/support/include/support/diagnostic/list.hh
+++ lib/support/include/support/diagnostic/list.hh
@@ -4,8 +4,8 @@
 
 #include <gsl/span>
 #include <stdx/iterator.hh>
-#include <stdx/option.hh>
 #include <stdx/utility.hh>
+#include <stdx/types.hh>
 
 #include "support/diagnostic/error.hh"
 
@@ -16,30 +16,19 @@ class diagnostic_list {
     MAKE_ITERATOR(list_t, std::vector<diagnostic>, list_)
 
   public:
-    constexpr explicit diagnostic_list(stdx::tribool in_terminal = stdx::none) noexcept
-        : in_terminal_{in_terminal} {}
+    diagnostic_list() = default;
     ~diagnostic_list() = default;
     MAKE_MOVE_ONLY(diagnostic_list);
 
-    template <typename... Args> auto emplace_back(Args&&... args) -> void {
-        list_.emplace_back(std::forward<Args>(args)...);
+    auto emplace_back(error err, usize line, usize column) -> void {
+        list_.emplace_back(err, line, column);
     }
 
     [[nodiscard]] constexpr operator gsl::span<diagnostic>() noexcept { return list_; }
     [[nodiscard]] constexpr operator gsl::span<const diagnostic>() const noexcept { return list_; }
 
-    // Creates a new list with the same terminal behavior
-    [[nodiscard]] auto create_new() const -> diagnostic_list {
-        return diagnostic_list{in_terminal_};
-    }
-
-    [[nodiscard]] auto get_terminal_status() const noexcept -> stdx::option<bool> {
-        return in_terminal_;
-    }
-
   private:
-    list_t        list_;
-    stdx::tribool in_terminal_;
+    list_t list_;
 };
 
 } // namespace cairn
