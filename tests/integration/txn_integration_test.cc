4459c4f513ee92510464ad9c016d74f202752fda
trevorswan11 <tcs94@case.edu>
2026-07-16 20:49:09 -0400

    refactor: Stop using namsepace of main library in integration tests

--- tests/integration/txn_integration_test.cc
+++ tests/integration/txn_integration_test.cc
@@ -27,12 +27,11 @@
 
 namespace cairn::tests {
 
-using namespace cairn::txn;
 using namespace stdx::size_literals;
 using namespace helpers::common_tids;
 using helpers::deterministic_scheduler;
 
-using txn_tree_t = iot_tree<i64, 128, 64>;
+using txn_tree_t = txn::iot_tree<i64, 128, 64>;
 using pool_t     = storage::buffer_pool<64>;
 
 TEST_CASE(
@@ -44,16 +43,16 @@ TEST_CASE(
     auto              pool{UNWRAP(pool_t::open(db_file.path))};
     pool->set_log_manager(lm);
 
-    auto                   base_tree{UNWRAP(txn_tree_t::tree_t::create(*pool))};
-    undo::manager<i64, 64> undo_mgr{*pool};
-    txn_tree_t             tree{base_tree, undo_mgr};
-    manager                tm;
-    lock::manager          lm_lock;
+    auto                        base_tree{UNWRAP(txn_tree_t::tree_t::create(*pool))};
+    txn::undo::manager<i64, 64> undo_mgr{*pool};
+    txn_tree_t                  tree{base_tree, undo_mgr};
+    txn::manager                tm;
+    txn::lock::manager          lm_lock;
     tm.set_lock_manager(lm_lock);
 
     // Initialize key 1 with "init_val"
     const std::string_view key1_init_val{"init_val"};
-    const auto             init_txn{tm.begin_txn(isolation_level_t::SNAPSHOT)};
+    const auto             init_txn{tm.begin_txn(txn::isolation_level_t::SNAPSHOT)};
     REQUIRE(tree.insert_txn(init_txn, 1, helpers::span_from_string(key1_init_val)));
     REQUIRE(tm.update_txn_lsn(init_txn, wal::log::seq_num{1}));
     REQUIRE(tm.commit_txn(init_txn, lm));
@@ -67,7 +66,7 @@ TEST_CASE(
 
     {
         std::jthread t0{[&] {
-            const auto t1{tm.begin_txn(isolation_level_t::SERIALIZABLE)};
+            const auto t1{tm.begin_txn(txn::isolation_level_t::SERIALIZABLE)};
             MT_REQUIRE(verifier, scheduler.yield_to_schedule(tid0));
 
             const auto             snap1{MT_UNWRAP(verifier, tm.acquire_snapshot(t1))};
@@ -90,7 +89,7 @@ TEST_CASE(
 
         std::jthread t1{[&] {
             MT_REQUIRE(verifier, scheduler.yield_to_schedule(tid1));
-            const auto t2{tm.begin_txn(isolation_level_t::SERIALIZABLE)};
+            const auto t2{tm.begin_txn(txn::isolation_level_t::SERIALIZABLE)};
 
             const auto             snap2{MT_UNWRAP(verifier, tm.acquire_snapshot(t2))};
             std::vector<std::byte> buf;
@@ -118,7 +117,7 @@ TEST_CASE(
 
     // Verify database state after rollback
     {
-        const auto             check_txn{tm.begin_txn(isolation_level_t::SNAPSHOT)};
+        const auto             check_txn{tm.begin_txn(txn::isolation_level_t::SNAPSHOT)};
         const auto             check_snap{UNWRAP(tm.acquire_snapshot(check_txn))};
         std::vector<std::byte> buf;
 
@@ -129,13 +128,13 @@ TEST_CASE(
 
     // Verify that locks were correctly released by executing a new transaction on key 1
     {
-        const auto t3{tm.begin_txn(isolation_level_t::SERIALIZABLE)};
+        const auto t3{tm.begin_txn(txn::isolation_level_t::SERIALIZABLE)};
         REQUIRE(tree.update_txn(t3, 1, helpers::span_from_string(t3_val)));
         REQUIRE(tm.update_txn_lsn(t3, wal::log::seq_num{4}));
         REQUIRE(tm.commit_txn(t3, lm));
 
         // Verify the update succeeded
-        const auto             check_txn{tm.begin_txn(isolation_level_t::SNAPSHOT)};
+        const auto             check_txn{tm.begin_txn(txn::isolation_level_t::SNAPSHOT)};
         const auto             check_snap{UNWRAP(tm.acquire_snapshot(check_txn))};
         std::vector<std::byte> buf;
         auto                   val{UNWRAP(tree.get_txn(check_txn, check_snap, tm, 1, buf))};
