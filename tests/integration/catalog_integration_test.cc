d38d812e21c601ed9ae191b5ca80123fa0d472a6
trevorswan11 <tcs94@case.edu>
2026-07-16 20:49:09 -0400

    refactor: Stop using namsepace of main library in integration tests

--- tests/integration/catalog_integration_test.cc
+++ tests/integration/catalog_integration_test.cc
@@ -21,7 +21,6 @@
 
 namespace cairn::tests {
 
-using namespace cairn::sql;
 using namespace stdx::size_literals;
 
 TEST_CASE("sql::catalog bootstrapping and metadata persistence") {
@@ -41,7 +40,7 @@ TEST_CASE("sql::catalog bootstrapping and metadata persistence") {
         pool->set_log_manager(lm);
         txn::undo::manager<i64, 64> undo_mgr{*pool};
         txn::manager                tm;
-        catalog<64>                 cat{*pool, tm, undo_mgr, lm};
+        sql::catalog<64>            cat{*pool, tm, undo_mgr, lm};
         REQUIRE(cat.bootstrap());
 
         saved_tables_root = cat.sys_tables_root();
@@ -52,16 +51,16 @@ TEST_CASE("sql::catalog bootstrapping and metadata persistence") {
         CHECK(saved_indexes_root != storage::INVALID_PAGE_ID);
 
         // Verify system tables exist
-        auto sys_tables{UNWRAP(cat.get_table(metadata::sys_tables_name))};
-        CHECK(sys_tables.name == metadata::sys_tables_name);
+        auto sys_tables{UNWRAP(cat.get_table(sql::metadata::sys_tables_name))};
+        CHECK(sys_tables.name == sql::metadata::sys_tables_name);
         CHECK(sys_tables.table_schema.column_count() == 3);
 
-        auto sys_columns{UNWRAP(cat.get_table(metadata::sys_columns_name))};
-        CHECK(sys_columns.name == metadata::sys_columns_name);
+        auto sys_columns{UNWRAP(cat.get_table(sql::metadata::sys_columns_name))};
+        CHECK(sys_columns.name == sql::metadata::sys_columns_name);
         CHECK(sys_columns.table_schema.column_count() == 4);
 
-        auto sys_indexes{UNWRAP(cat.get_table(metadata::sys_indexes_name))};
-        CHECK(sys_indexes.name == metadata::sys_indexes_name);
+        auto sys_indexes{UNWRAP(cat.get_table(sql::metadata::sys_indexes_name))};
+        CHECK(sys_indexes.name == sql::metadata::sys_indexes_name);
         CHECK(sys_indexes.table_schema.column_count() == 5);
     }
 
@@ -72,16 +71,16 @@ TEST_CASE("sql::catalog bootstrapping and metadata persistence") {
         txn::undo::manager<i64, 64> undo_mgr{*pool};
         txn::manager                tm;
         tm.set_next_txn_id(txn::id_t{10});
-        catalog<64> cat{*pool, tm, undo_mgr, lm};
+        sql::catalog<64> cat{*pool, tm, undo_mgr, lm};
         REQUIRE(cat.bootstrap());
 
         CHECK(cat.sys_tables_root() == saved_tables_root);
         CHECK(cat.sys_columns_root() == saved_columns_root);
         CHECK(cat.sys_indexes_root() == saved_indexes_root);
 
-        auto sys_tables{cat.get_table(metadata::sys_tables_name)};
+        auto sys_tables{cat.get_table(sql::metadata::sys_tables_name)};
         REQUIRE(sys_tables.has_value());
-        CHECK(sys_tables->name == metadata::sys_tables_name);
+        CHECK(sys_tables->name == sql::metadata::sys_tables_name);
     }
 }
 
@@ -98,15 +97,15 @@ TEST_CASE("sql::catalog restart persistence of user metadata") {
         pool->set_log_manager(lm);
         txn::undo::manager<i64, 64> undo_mgr{*pool};
         txn::manager                tm;
-        catalog<64>                 cat{*pool, tm, undo_mgr, lm};
+        sql::catalog<64>            cat{*pool, tm, undo_mgr, lm};
         REQUIRE(cat.bootstrap());
 
-        const auto transaction{tm.begin_txn()};
-        schema     sch{
-                {column{"key", type::id_t::BIGINT, false}, column{"value", type::id_t::VARCHAR, true}}};
-        REQUIRE(cat.create_table(transaction, table_id_t{42}, "items", sch));
+        const auto  transaction{tm.begin_txn()};
+        sql::schema sch{{sql::column{"key", sql::type::id_t::BIGINT, false},
+                         sql::column{"value", sql::type::id_t::VARCHAR, true}}};
+        REQUIRE(cat.create_table(transaction, sql::table_id_t{42}, "items", sch));
         REQUIRE(cat.create_index(
-            transaction, table_id_t{42}, index_id_t{99}, "idx_items_key", 0, false));
+            transaction, sql::table_id_t{42}, sql::index_id_t{99}, "idx_items_key", 0, false));
 
         REQUIRE(tm.update_txn_lsn(transaction, wal::log::seq_num{2}));
         REQUIRE(tm.commit_txn(transaction, lm));
@@ -119,22 +118,22 @@ TEST_CASE("sql::catalog restart persistence of user metadata") {
         txn::undo::manager<i64, 64> undo_mgr{*pool};
         txn::manager                tm;
         tm.set_next_txn_id(txn::id_t{10});
-        catalog<64> cat{*pool, tm, undo_mgr, lm};
+        sql::catalog<64> cat{*pool, tm, undo_mgr, lm};
         REQUIRE(cat.bootstrap());
 
         // Verify table persisted
         auto table{UNWRAP(cat.get_table("items"))};
-        CHECK(table.table_id == table_id_t{42});
+        CHECK(table.table_id == sql::table_id_t{42});
         CHECK(table.table_schema.column_count() == 2);
         CHECK(table.table_schema[0].name() == "key");
-        CHECK(table.table_schema[0].type() == type::id_t::BIGINT);
+        CHECK(table.table_schema[0].type() == sql::type::id_t::BIGINT);
         CHECK(table.table_schema[1].name() == "value");
-        CHECK(table.table_schema[1].type() == type::id_t::VARCHAR);
+        CHECK(table.table_schema[1].type() == sql::type::id_t::VARCHAR);
 
         // Verify index persisted
         auto idx{UNWRAP(cat.get_index("idx_items_key"))};
-        CHECK(idx.table_id == table_id_t{42});
-        CHECK(idx.index_id == index_id_t{99});
+        CHECK(idx.table_id == sql::table_id_t{42});
+        CHECK(idx.index_id == sql::index_id_t{99});
         CHECK(idx.column_id == 0);
         CHECK(idx.is_unique == false);
     }
