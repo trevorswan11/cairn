d71ca47ab2f5194bf891838eca4286e5971b3826
trevorswan11 <tcs94@case.edu>
2026-07-22 20:29:42 -0400

    chore: format env file

--- lib/testhelpers/include/testhelpers/env.hh
+++ lib/testhelpers/include/testhelpers/env.hh
@@ -7,7 +7,7 @@
 
 namespace cairn::tests::helpers {
 
-auto set_env(const std::string& name, const std::string& value) -> void;
+auto               set_env(const std::string& name, const std::string& value) -> void;
 [[nodiscard]] auto get_env(const std::string& name) -> stdx::option<std::string_view>;
 
 } // namespace cairn::tests::helpers
