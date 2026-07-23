e27a9893783fe626c5897539055f97c4b115d0c9
trevorswan11 <tcs94@case.edu>
2026-07-23 19:25:14 -0400

    feat: add cast value helper to evaluator

--- lib/exec/include/exec/expression_evaluator.hh
+++ lib/exec/include/exec/expression_evaluator.hh
@@ -3,15 +3,24 @@
 #include <string>
 #include <vector>
 
+#include <stdx/assert.hh>
+#include <stdx/result.hh>
 #include <stdx/variant.hh>
 
 #include "sql/binder/nodes.hh"
 #include "sql/schema.hh"
 #include "sql/tuple.hh"
+#include "sql/type.hh"
 #include "sql/value.hh"
 #include "support/diagnostic/error.hh"
 
 namespace cairn::exec {
+
+#define EXPR_EVAL_NOOP(type)                                                \
+    auto operator()(sql::binder::node_id_t, const type&, const sql::tuple&) \
+        ->result<sql::value_t> {                                            \
+        return stdx::err{error::SQL_UNEXPECTED_NODE};                       \
+    }
 
 class expression_evaluator_t {
   public:
@@ -25,6 +34,9 @@ class expression_evaluator_t {
     }
 
   private:
+    [[nodiscard]] auto cast_value(const sql::value_t& val, sql::type::id_t target_type)
+        -> result<sql::value_t>;
+
     [[nodiscard]] auto operator()(sql::binder::node_id_t id,
                                   const stdx::monostate& node,
                                   const sql::tuple&      input_tuple) -> result<sql::value_t>;
@@ -40,38 +52,27 @@ class expression_evaluator_t {
     [[nodiscard]] auto operator()(sql::binder::node_id_t          id,
                                   const sql::binder::cast_expr_t& node,
                                   const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t               id,
-                                  const sql::binder::aggregate_expr_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
     [[nodiscard]] auto operator()(sql::binder::node_id_t           id,
                                   const sql::binder::unary_expr_t& node,
                                   const sql::tuple& input_tuple) -> result<sql::value_t>;
     [[nodiscard]] auto operator()(sql::binder::node_id_t              id,
                                   const sql::binder::function_expr_t& node,
                                   const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t            id,
-                                  const sql::binder::select_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t                  id,
-                                  const sql::binder::create_table_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t                id,
-                                  const sql::binder::drop_table_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t                 id,
-                                  const sql::binder::alter_table_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t                  id,
-                                  const sql::binder::create_index_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
-    [[nodiscard]] auto operator()(sql::binder::node_id_t                id,
-                                  const sql::binder::drop_index_stmt_t& node,
-                                  const sql::tuple& input_tuple) -> result<sql::value_t>;
+
+    EXPR_EVAL_NOOP(sql::binder::aggregate_expr_t)
+    EXPR_EVAL_NOOP(sql::binder::select_stmt_t)
+    EXPR_EVAL_NOOP(sql::binder::create_table_stmt_t)
+    EXPR_EVAL_NOOP(sql::binder::drop_table_stmt_t)
+    EXPR_EVAL_NOOP(sql::binder::alter_table_stmt_t)
+    EXPR_EVAL_NOOP(sql::binder::create_index_stmt_t)
+    EXPR_EVAL_NOOP(sql::binder::drop_index_stmt_t)
 
   private:
-    [[maybe_unused]] const sql::binder::ast_t& ast_;
-    [[maybe_unused]] const sql::schema&        sch_;
-    std::vector<std::string>                   string_pool_;
+    const sql::binder::ast_t& ast_;
+    const sql::schema&        sch_;
+    std::vector<std::string>  string_pool_;
 };
+
+#undef EXPR_EVAL_NOOP
 
 } // namespace cairn::exec
